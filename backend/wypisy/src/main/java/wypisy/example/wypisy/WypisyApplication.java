685d7f318f1f6f277122640f173a7cb0750b469b
MarvelSas <marcinkk543@gmail.com>
2024-02-19 21:16:19 +0100

    Add backend.

---
+++ backend/wypisy/src/main/java/wypisy/example/wypisy/WypisyApplication.java
@@ -0,0 +1,13 @@
+package wypisy.example.wypisy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WypisyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WypisyApplication.class, args);
+	}
+
+}
---
+++ backend/wypisy/src/test/java/wypisy/example/wypisy/WypisyApplicationTests.java
@@ -0,0 +1,13 @@
+package wypisy.example.wypisy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WypisyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
