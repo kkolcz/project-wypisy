039edcaddd61e3156978ce2730a67f922e40df29
kkolcz <vipciorr@gmail.com>
2024-02-28 21:22:14 +0100

    add login api

--- frontend/src/app/services/user-auth.service.ts
+++ frontend/src/app/services/user-auth.service.ts
@@ -3,6 +3,7 @@ import { Observable, Subject, Subscription } from 'rxjs';
 
 import { User } from '../models/user.model';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,9 @@ import { Router } from '@angular/router';
 export class UserAuthService implements OnInit {
   userIsLoginSubject = new Subject<boolean>();
   userIsLogin: boolean = false;
+  token: string = null;
+
+  API_URL = 'http://localhost:8080/api/v1';
 
   userLogin: User = {
     id: 1,
@@ -19,7 +23,7 @@ export class UserAuthService implements OnInit {
     token: 'token:45234234',
   };
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   isLogin() {
     return this.userIsLogin;
@@ -28,9 +32,22 @@ export class UserAuthService implements OnInit {
   signIn(loginData) {
     this.userIsLogin = true;
     this.userIsLoginSubject.next(true);
-    console.log(loginData);
-    if (true) {
+    // console.log(loginData);
+
+    const data = { email: loginData.email, password: loginData.password };
+    console.log(data);
+    this.http
+      .post(`${this.API_URL}/auth/authenticate`, data)
+      .subscribe((res) => {
+        console.log(res);
+      });
+
+    const tempToken = 'token:45234234';
+
+    if (false) {
       this.router.navigate(['/']);
+      this.token = tempToken;
+      localStorage.setItem('token', tempToken);
     }
   }
 
